4e93b0fe140e4498994cd74d627373baf4d88a29
Wilson Birch <wilsonbirch@gmail.com>
2020-10-05 19:23:11 -0400

    Updates

---
+++ precache-manifest.cfc9954eb2d36bfe915dc718c3e67b10.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751ae09f17c100c10b3bd63139adbe7a",
+    "url": "/React_Employee_Directory/index.html"
+  },
+  {
+    "revision": "5c4ca5dd5b76e009154e",
+    "url": "/React_Employee_Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "83ce2734a5ef42425e7e",
+    "url": "/React_Employee_Directory/static/css/main.abaa3831.chunk.css"
+  },
+  {
+    "revision": "5c4ca5dd5b76e009154e",
+    "url": "/React_Employee_Directory/static/js/2.aecf6c5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Employee_Directory/static/js/2.aecf6c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ce2734a5ef42425e7e",
+    "url": "/React_Employee_Directory/static/js/main.cf744f99.chunk.js"
+  },
+  {
+    "revision": "501cc3f783551c32c58c",
+    "url": "/React_Employee_Directory/static/js/runtime-main.0bd013b3.js"
+  },
+  {
+    "revision": "e55d17c19d4c0b38e1757b2b25da98f0",
+    "url": "/React_Employee_Directory/static/media/creedbratton.e55d17c1.jpg"
+  },
+  {
+    "revision": "0a5e4a55199151f1bc8b41565afba02f",
+    "url": "/React_Employee_Directory/static/media/dwightschrute.0a5e4a55.jpeg"
+  },
+  {
+    "revision": "f6784645e134c36e72f8866b73b9aca4",
+    "url": "/React_Employee_Directory/static/media/janlevinson.f6784645.jpg"
+  },
+  {
+    "revision": "7b0a1fdac61f06cf0695c4693f36bf41",
+    "url": "/React_Employee_Directory/static/media/jimhalpert.7b0a1fda.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_Employee_Directory/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bd5258c36775ffd3357c4609b7494887",
+    "url": "/React_Employee_Directory/static/media/prisonmike.bd5258c3.png"
+  },
+  {
+    "revision": "84b0b428abe934c01d6ffdacadc61873",
+    "url": "/React_Employee_Directory/static/media/robertcalifornia.84b0b428.jpg"
+  },
+  {
+    "revision": "980c96c089b5ea16b83d00e3353ebca2",
+    "url": "/React_Employee_Directory/static/media/ryanhoward.980c96c0.jpg"
+  },
+  {
+    "revision": "722339c052cd28c760531903caf348c1",
+    "url": "/React_Employee_Directory/static/media/stanleyhudson.722339c0.png"
+  }
+]);
