a686e056891c5609045d415c7fc5c980a253d36d
Wilson Birch <wilsonbirch@gmail.com>
2020-10-05 01:33:50 -0400

    Updates

---
+++ precache-manifest.3f79d85a5a2b15c795771ef293dc56df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa424cca6068445b2681a2d027f5dae1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b40a7e48aeb3a8e36a7b",
+    "url": "/my-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "504347fdb0c77dd3f79e",
+    "url": "/my-app/static/css/main.abaa3831.chunk.css"
+  },
+  {
+    "revision": "b40a7e48aeb3a8e36a7b",
+    "url": "/my-app/static/js/2.9ea03d95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.9ea03d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504347fdb0c77dd3f79e",
+    "url": "/my-app/static/js/main.d6bb114c.chunk.js"
+  },
+  {
+    "revision": "b3505c3d477b4db463a1",
+    "url": "/my-app/static/js/runtime-main.958628b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
