8b93d905413eabcdff1ee4b7f9c4aa20765e99d0
Wilson Birch <wilsonbirch@gmail.com>
2020-10-05 01:26:27 -0400

    Combined a few componenets for filter funtionality by name of employees

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
-import Table from './components/Table'
 import Wrapper from './components/Wrapper'
-import SearchBar from './components/SearchBar'
+import Table from './components/Table'
+
 
 
 function App() {
@@ -11,7 +11,6 @@ function App() {
     return (
       <Wrapper>
             <Header></Header>
-            <SearchBar></SearchBar>
             <Table></Table>
       </Wrapper>
     );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,68 @@
-import React from "react";
-//import Employees from "public/employees.json"
+import React, {useState} from "react";
 import employeeJson from '../employees.json'
+
 
 function Table(props) {
 
-    const employeeItems = employeeJson.employees.map(employee => 
-        <tr>
-        <th scope ="row">{employee.id}</th>
-        <td class ="w-25"> <img src= {employee.photo} class ="img-fluid img-thumbnail" alt = {employee.name}></img></td>
-        <td>{employee.name}</td>
-        <td>{employee.position}</td>
-        <td>{employee.email}</td>
-    </tr>)
+    let employeeItems, i=0, renderEmployeeArray = [];
+
+
+    const [searchName, setSearchName] = useState({
+        name: ""
+    })
+
+    const handleSubmit = e => {
+        e.preventDefault();
+ 
+    }
+
+    const handleChange = e => {
+        setSearchName({ ...searchName, name: e.target.value})
+        //console.log(searchName.name);
+    }
+
+    if (searchName.name === "") {
+         employeeItems = employeeJson.employees.map(employee => 
+
+            <tr key = {employee.id}>
+            <th  scope ="row">{employee.id}</th>
+            <td className ="w-25"> <img src= {employee.photo} className ="img-fluid img-thumbnail" alt = {employee.name}></img></td>
+            <td>{employee.name}</td>
+            <td>{employee.position}</td>
+            <td>{employee.email}</td>
+        </tr>
+        
+    )} else {
+        for (i=0; i< employeeJson.employees.length; i++) {
+            if ((employeeJson.employees[i].name).includes(searchName.name)) {
+                renderEmployeeArray[i] = employeeJson.employees[i];
+            }
+        }
+
+        employeeItems = renderEmployeeArray.map(employee => 
+
+            <tr key = {employee.id}>
+            <th  scope ="row">{employee.id}</th>
+            <td className ="w-25"> <img src= {employee.photo} className ="img-fluid img-thumbnail" alt = {employee.name}></img></td>
+            <td>{employee.name}</td>
+            <td>{employee.position}</td>
+            <td>{employee.email}</td>
+        </tr>
+        )}
+
 
     return (
         <div className = "container">
+            <div className ="row">
+                <div className="col-4"></div>
+                    <form className="form-inline" onSubmit={handleSubmit}>
+                        <div className="form-group mx-sm-4 mb-2">
+                            <input type="name" className="form-control" id="inputSearchName" placeholder="Name (Case Sensitive)" onChange={handleChange}></input>
+                        </div>
+                        <button type="submit" className="btn btn-primary mb-2">Filter</button>
+                    </form>
+            </div>
+
             <table className="table">
                 <thead>
                     <tr>
@@ -30,6 +78,8 @@ function Table(props) {
                 </tbody>
             </table>
         </div>
+
+        
     );
 }
 
